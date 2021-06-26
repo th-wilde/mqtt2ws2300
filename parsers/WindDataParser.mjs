73e74b923eb1f1fe8426d55c690069a6da286dbd
Thorben Wilde <thorben.wilde@gmx.de>
2021-06-26 18:19:03 +0100

    Also report wind speed as km/h

--- parsers/WindDataParser.mjs
+++ parsers/WindDataParser.mjs
@@ -15,7 +15,7 @@ class WindDataPaser extends WS2300Parser {
     process(data) {
         //Nibbel 5 umd 4 und 3 ergänzen und Dezimalwert durch 10 teilen.
         var calcms = (((data[2] & 0xF) << 8) + (data[1])) / 10.0;
-
+	var calckmh = calcms * 60 * 60;
         //Nibbel 6 * 22.5 (22.5 weil 360/16 [Ein Nibbel = 4 Bit = 16 Zustände])
         //WinkelGrad von Norden aus im Uhrzeigersinn
 		var direction = data[2] >> 4;
@@ -24,7 +24,8 @@ class WindDataPaser extends WS2300Parser {
         //Ungülige Wind-Daten erkennen
         if ((data[0] == 0x00) && ((data[1] != 0xFF) || (((data[2] & 0xF) != 0) && ((data[2] & 0xF) != 1)))) {
             this.lastWindSpeed = calcms;
-            this.mqttPublish("WindSpeed", calcms.toFixed(1));
+            this.mqttPublish("WindSpeedMs", calcms.toFixed(1));
+            this.mqttPublish("WindSpeedKmh", calckmh.toFixed(1));
             this.mqttPublish("WindDirectionDeg", caldeg.toFixed(1));
 			switch (direction){
 				case 0:
